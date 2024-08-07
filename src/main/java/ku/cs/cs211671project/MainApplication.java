36d49a085d769a2c4ea095196947ccdbab34a86a
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-08-07 13:15:10 +0700

    update MainApplication - config department page routes & add Application font loader

--- src/main/java/ku/cs/cs211671project/MainApplication.java
+++ src/main/java/ku/cs/cs211671project/MainApplication.java
@@ -3,6 +3,7 @@ package ku.cs.cs211671project;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import ku.cs.services.FXRouter;
 
@@ -11,14 +12,25 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        String fontsPath = "/fonts/";
+        Font.loadFont(getClass().getResourceAsStream(fontsPath + "PrintAble4U Regular ver 1.00.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream(fontsPath + "PrintAble4U Bold ver 1.00.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream(fontsPath + "PrintAble4U Italic ver 1.00.ttf"), 12);
+        Font.loadFont(getClass().getResourceAsStream(fontsPath + "PrintAble4U Bold Italic ver 1.00.ttf"), 12);
         FXRouter.bind(this, stage, "CS211 Project", 1280, 720);
         configRoutes();
-        FXRouter.goTo("hello");
+        FXRouter.goTo("department-staff-request-list");
     }
 
     private void configRoutes() {
         String viewPath = "ku/cs/views/";
         FXRouter.when("hello", viewPath + "hello-view.fxml");
+        FXRouter.when("department-staff-request-list", viewPath + "department-staff-request-list.fxml");
+        FXRouter.when("department-staff-request", viewPath + "department-staff-request.fxml");
+        FXRouter.when("department-staff-approver-list", viewPath + "department-staff-approver-list.fxml");
+        FXRouter.when("department-staff-nisit-advisor-management", viewPath + "department-staff-nisit-advisor-management.fxml");
+        FXRouter.when("department-staff-nisit-management", viewPath + "department-staff-nisit-management.fxml");
+
     }
 
     public static void main(String[] args) {
