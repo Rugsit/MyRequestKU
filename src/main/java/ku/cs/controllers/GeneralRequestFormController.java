3417f8ee67efaefe9be8d61660bf4dcd8ef1ca11
Rugsit <rugsit.nest@gmail.com>
2024-08-29 08:51:46 +0700

    Link student page to choose request form and Edit some style in student page

--- src/main/java/ku/cs/controllers/GeneralRequestFormController.java
+++ src/main/java/ku/cs/controllers/GeneralRequestFormController.java
@@ -1,10 +1,15 @@
 package ku.cs.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
 
 public class GeneralRequestFormController {
     @FXML
@@ -55,5 +60,25 @@ public class GeneralRequestFormController {
     @FXML
     private TextField telTextField;
 
+    @FXML
+    public BorderPane borderPane;
 
+    public void setBorderPane(BorderPane borderPane) {
+        this.borderPane = borderPane;
+    }
+
+    @FXML
+    public void onBackButtonClick() {
+        try {
+            String viewPath = "/ku/cs/views/choose-request-form-pane.fxml";
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource(viewPath));
+            Pane pane = fxmlLoader.load();
+            ChooseRequestFromController controller = fxmlLoader.getController();
+            controller.setBorderPane(this.borderPane);
+            borderPane.setCenter(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
