9466aa4cd56423773b7749f0f1b1d26faa1ecbdf
Rugsit <rugsit.nest@gmail.com>
2024-09-11 21:29:37 +0700

    Fix request list index in add request method

--- src/main/java/ku/cs/models/request/RequestList.java
+++ src/main/java/ku/cs/models/request/RequestList.java
@@ -18,16 +18,16 @@ public class RequestList {
             case "General":
                 request = new GeneralRequestForm(data); break;
             case "AcademicLeave":
-                String[] first = Arrays.copyOfRange(data, 0, 19);
-                String[] second = Arrays.copyOfRange(data, 19, data.length);
+                String[] first = Arrays.copyOfRange(data, 0, 20);
+                String[] second = Arrays.copyOfRange(data, 20, data.length);
                 request = new AcademicLeaveRequestForm(first, second); break;
             case "KU1":
-                String[] firstKU1 = Arrays.copyOfRange(data, 0, 13);
-                String[] secondKU1 = Arrays.copyOfRange(data, 13, data.length);
+                String[] firstKU1 = Arrays.copyOfRange(data, 0, 14);
+                String[] secondKU1 = Arrays.copyOfRange(data, 14, data.length);
                 request = new Ku1AndKu3RequestForm(firstKU1, secondKU1, (byte)1); break;
             case "KU3":
-                String[] firstKU3 = Arrays.copyOfRange(data, 0, 13);
-                String[] secondKU3 = Arrays.copyOfRange(data, 13, data.length);
+                String[] firstKU3 = Arrays.copyOfRange(data, 0, 14);
+                String[] secondKU3 = Arrays.copyOfRange(data, 14, data.length);
                 request = new Ku1AndKu3RequestForm(firstKU3, secondKU3, (byte)3);break;
             default:
                 request = null;
