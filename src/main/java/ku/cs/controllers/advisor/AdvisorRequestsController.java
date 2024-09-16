3083796e61c5282f37b3a952357cce1d8d4e9d5e
Tanaanan <mjsalyjoh@gmail.com>
2024-09-17 01:17:28 +0700

    Add listview and searchbar on requests-pane and students-pane

--- src/main/java/ku/cs/controllers/advisor/AdvisorRequestsController.java
+++ src/main/java/ku/cs/controllers/advisor/AdvisorRequestsController.java
@@ -1,21 +1,35 @@
 package ku.cs.controllers.advisor;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import ku.cs.controllers.requests.ChooseRequestFromController;
-
-import java.io.IOException;
 
 public class AdvisorRequestsController {
     @FXML TableView requestListTableView;
     @FXML
     BorderPane borderPane;
+
+    public void initialize() {
+        showTable();
+    }
+
+    private void showTable(){
+        requestListTableView.getColumns().clear();
+        TableColumn<String, String> nameColumn = new TableColumn<>("ชื่อ-นามสกุล");
+        TableColumn<String, String> dateColumn = new TableColumn<>("วันที่ยื่นคำร้อง");
+        TableColumn<String, String> typeColumn = new TableColumn<>("ประเภทคำร้อง");
+        TableColumn<String, String> statusColumn = new TableColumn<>("สถานะคำร้อง");
+        requestListTableView.getColumns().add(nameColumn);
+        requestListTableView.getColumns().add(dateColumn);
+        requestListTableView.getColumns().add(typeColumn);
+        requestListTableView.getColumns().add(statusColumn);
+        nameColumn.setMinWidth(150);
+        dateColumn.setMinWidth(200);
+        typeColumn.setMinWidth(200);
+        statusColumn.setMinWidth(381);
+    }
+
 
     public void setBorderPane(BorderPane borderPane) {
         this.borderPane = borderPane;
--- src/main/java/ku/cs/controllers/advisor/AdvisorStudentListController.java
+++ src/main/java/ku/cs/controllers/advisor/AdvisorStudentListController.java
@@ -1,21 +1,28 @@
 package ku.cs.controllers.advisor;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import ku.cs.controllers.requests.ChooseRequestFromController;
-
-import java.io.IOException;
 
 public class AdvisorStudentListController {
     @FXML TableView requestListTableView;
     @FXML
     BorderPane borderPane;
+
+    public void initialize() {
+        showTable();
+    }
+
+    private void showTable(){
+        requestListTableView.getColumns().clear();
+        TableColumn<String, String> nameColumn = new TableColumn<>("ชื่อ-นามสกุล");
+        TableColumn<String, String> idColumn = new TableColumn<>("รหัสนิสิต");
+        requestListTableView.getColumns().add(nameColumn);
+        requestListTableView.getColumns().add(idColumn);
+        nameColumn.setMinWidth(531);
+        idColumn.setMinWidth(400);
+    }
 
     public void setBorderPane(BorderPane borderPane) {
         this.borderPane = borderPane;
