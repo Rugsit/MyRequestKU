a564af53d5fd894a213a54c47e167a74821aafb1
Tanaanan <mjsalyjoh@gmail.com>
2024-09-18 21:00:14 +0700

    add feature display student-request after click in student-listview and handle previous bug on display student-requests

--- src/main/java/ku/cs/controllers/advisor/AdvisorStudentRequestsController.java
+++ src/main/java/ku/cs/controllers/advisor/AdvisorStudentRequestsController.java
@@ -1,11 +1,13 @@
 package ku.cs.controllers.advisor;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import ku.cs.models.request.Request;
 import ku.cs.models.request.RequestList;
 import ku.cs.models.user.Student;
@@ -15,6 +17,7 @@ import ku.cs.services.Datasource;
 import ku.cs.services.RequestListFileDatasource;
 import ku.cs.services.UserListFileDatasource;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 
@@ -91,6 +94,22 @@ public class AdvisorStudentRequestsController {
         }
     }
 
+
+    public void onBackButtonClick(){
+        try {
+            String viewPath = "/ku/cs/views/advisor-students-pane.fxml";
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource(viewPath));
+            Pane pane = fxmlLoader.load();
+            AdvisorStudentListController controller = fxmlLoader.getController();
+            controller.initialize();
+            borderPane.setCenter(pane);
+            controller.setBorderPane(borderPane);
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 
     public void setBorderPane(BorderPane borderPane) {
         this.borderPane = borderPane;
