68e94f0b3a1e53680acace1a35d2e7227ef62325
nrkn <narakorn458@gmail.com>
2024-09-19 23:58:53 +0700

    edit setName and add updateUsers to Faculty to make sure faculty fields in users are updated

--- src/main/java/ku/cs/models/faculty/Faculty.java
+++ src/main/java/ku/cs/models/faculty/Faculty.java
@@ -2,8 +2,13 @@ package ku.cs.models.faculty;
 
 import ku.cs.models.department.Department;
 import ku.cs.models.department.DepartmentList;
+import ku.cs.models.user.FacultyUser;
+import ku.cs.models.user.User;
+import ku.cs.models.user.UserList;
+import ku.cs.models.user.exceptions.UserException;
 import ku.cs.services.Datasource;
 import ku.cs.services.DepartmentListFileDatasource;
+import ku.cs.services.UserListFileDatasource;
 
 import java.util.HashSet;
 import java.util.UUID;
@@ -11,7 +16,7 @@ import java.util.UUID;
 public class Faculty {
     private String name;
     private String id;
-    private UUID uuid;
+    private final UUID uuid;
 
     public Faculty(String name, String id) {
         try {
@@ -37,7 +42,9 @@ public class Faculty {
         if (name.isEmpty()){
             throw new IllegalArgumentException("กรุณาใส่ชื่อคณะให้ถูกต้อง");
         }
+        String oldName = this.name;
         this.name = name;
+        updateUsers(oldName);
     }
 
     public void setId(String id) {
@@ -75,5 +82,27 @@ public class Faculty {
     @Override
     public String toString() {
         return name + "," + id + "," + uuid;
+    }
+
+    public void updateUsers(String name) {
+        if (name == null || name.equals(this.name)){ return; }
+        DepartmentList departments = getDepartmentsByFaculty();
+        for (Department department : departments.getDepartments()) {
+            department.updateUsers(this.name, "faculty");
+        }
+
+        Datasource<UserList> datasource = new UserListFileDatasource("data", "faculty-staff.csv");
+        UserList facultyStaff = datasource.readData();
+        for (User user : facultyStaff.getUsers()) {
+            if ( ((FacultyUser) user).getFaculty().equals(name) ){
+                try {
+                    ((FacultyUser) user).setFaculty(this.name);
+                } catch (UserException e) {
+                    System.err.println("error changing users' faculty");
+                }
+            }
+        }
+        datasource.writeData(facultyStaff);
+
     }
 }
