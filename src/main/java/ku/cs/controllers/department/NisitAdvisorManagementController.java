b684ef4e861c3ab5d59c012a43e86df51eeee2d1
Sirisuk Tharntham <79169700+punsnx@users.noreply.github.com>
2024-09-20 01:47:14 +0700

    update - add feature department-nisit-link-advisor

--- src/main/java/ku/cs/controllers/department/NisitAdvisorManagementController.java
+++ src/main/java/ku/cs/controllers/department/NisitAdvisorManagementController.java
@@ -1,15 +1,26 @@
 package ku.cs.controllers.department;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import ku.cs.models.Session;
+import ku.cs.models.user.*;
+import ku.cs.models.user.exceptions.UserException;
+import ku.cs.services.FXRouter;
+import ku.cs.services.ImageDatasource;
+import ku.cs.services.UserListFileDatasource;
 import ku.cs.views.components.*;
+
+import java.util.UUID;
 
 public class NisitAdvisorManagementController {
     @FXML private Label pageTitleLabel;
@@ -22,9 +33,9 @@ public class NisitAdvisorManagementController {
     @FXML private HBox advisorTableHeaderHBox;
 
     @FXML private Label nisitTableViewLabel;
-    @FXML private TableView<?> nisitTableView;
+    @FXML private TableView<User> nisitTableView;
     @FXML private Label advisorTableViewLabel;
-    @FXML private TableView<?> advisorTableView;
+    @FXML private TableView<User> advisorTableView;
 
     @FXML private VBox showNisitVBox;
     @FXML private VBox showAdvisorVBox;
@@ -49,19 +60,51 @@ public class NisitAdvisorManagementController {
 
     @FXML private Button changeAdvisorButton;
     @FXML private Button backButton;
+    @FXML private StackPane mainStackPane;
+
+    private UserListFileDatasource nisitDatasource;
+    private UserListFileDatasource advisorDatasource;
+    private UserList nisitList;
+    private UserList advisorList;
+    private Student selectedStudent;
+    private Advisor selectedAdvisor;
+    private Session session;
+
+    private void initRouteData(){
+        Object object = FXRouter.getData();
+        if(object instanceof Session){
+            this.session = (Session) object;
+        }else{
+            session = null;
+        }
+    }
 
     @FXML
-    public void initialize(){
-        initLabel();
-        initButton();
+    public void initialize() {
+        initRouteData();
+        nisitDatasource = new UserListFileDatasource("data","student.csv");
+        advisorDatasource = new UserListFileDatasource("data","advisor.csv");
+        selectedStudent = null;
+        selectedAdvisor = null;
+
+        initNisitTableView();
+        initAdvisorTableView();
+        refreshNisitTable();
+        refreshAdvisorTable();
+
+
         initShowVBox(showNisitVBox);
         initShowVBox(showAdvisorVBox);
-        Image image = new Image(getClass().getResourceAsStream("/images/profile-test.png"));
-        new SquareImage(nisitImageView,image).setClipImage(50,50);
-        new SquareImage(advisorImageView,image).setClipImage(50,50);;
-//        changeModelImageView(null, nisitImageView);
-//        changeModelImageView(null, advisorImageView);
+        showNisitVBox.setVisible(false);
+        showNisitVBox.setDisable(true);
+        showAdvisorVBox.setVisible(false);
+        showAdvisorVBox.setDisable(true);
+//        changeAdvisorButton.setVisible(false);
+        changeAdvisorButton.setDisable(true);
         showAdvisorVBox.setPadding(new Insets(20,0,0,0));
+
+        initLabel();
+        initButton();
     }
     private void initLabel(){
         new DefaultLabel(pageTitleLabel);
@@ -85,7 +128,327 @@ public class NisitAdvisorManagementController {
     }
     private void initButton(){
         new RouteButton(backButton,"department-staff-request-list","transparent","#a6a6a6","#000000");
-        new DefaultButton(changeAdvisorButton,"#FF4E4E","#D03E3E","#FFFFFF").changeBackgroundRadius(15);
+        DefaultButton changeAdvisorBT = new DefaultButton(changeAdvisorButton,"#FF4E4E","#D03E3E","#FFFFFF"){
+          @Override
+          protected void handleClickEvent(){
+              button.setOnMouseClicked(e -> {
+                  System.out.println(buttonName + "clicked!");
+                  mainStackPane.getChildren().add(new ConfirmStack("ยืนยัน","คุณต้องการเปลี่ยนอาจารย์ที่ปรึกษาใช่มั้ย"){
+                      @Override
+                      protected void handleAcceptButton(){
+                          getAcceptButton().setOnMouseClicked(e -> {
+                              System.out.println("Accept button clicked");
+                              mainStackPane.getChildren().removeLast();
+                              onChangeAdvisor();
+                          });
+                      }
+                      @Override
+                      protected void handleDeclineButton(){
+                          getDeclineButton().setOnMouseClicked(e -> {
+                              System.out.println("Decline button clicked");
+                              mainStackPane.getChildren().removeLast();
+                          });
+                      }
+                  });
+
+              });
+          }
+        };
+        changeAdvisorBT.changeBackgroundRadius(15);
+    }
+    private void initNisitTableView(){
+        DefaultTableView<User> nisitTable = new DefaultTableView(nisitTableView){
+            @Override
+            protected void handleCLick() {
+                getTableView().getSelectionModel().selectedItemProperty().addListener(new ChangeListener<User>() {
+                    @Override
+                    public void changed(ObservableValue<? extends User> observable, User oldValue, User newValue) {
+                        if (newValue != null) {
+                            selectedStudent = (Student) newValue;
+                        }
+                        selectedStudentListener();
+                    }
+                });
+            }
+        };
+        nisitTable.getTableView().getColumns().clear();
+        nisitTable.getTableView().getItems().clear();
+        nisitTable.addColumn("รหัสนิสิต","id");
+        nisitTable.addColumn("ชื่อ-นามสกุล","name");
+        nisitTable.addColumn("อีเมล","email");
+        nisitTable.addColumn("คณะ","faculty");
+        nisitTable.addColumn("ภาควิชา","department");
+        nisitTable.getTableView().getColumns().add(newStatusColumn("สถานะ"));
+        nisitTable.getTableView().getColumns().add(newDeleteColumn());
+        nisitTable.addStyleSheet("/ku/cs/styles/department/pages/nisit-advisor-management/department-nisit-advisor-management-nisit-table-stylesheet.css");
+    }
+    private void initAdvisorTableView(){
+        DefaultTableView<User> nisitTable = new DefaultTableView(advisorTableView){
+            @Override
+            protected void handleCLick() {
+                getTableView().getSelectionModel().selectedItemProperty().addListener(new ChangeListener<User>() {
+                    @Override
+                    public void changed(ObservableValue<? extends User> observable, User oldValue, User newValue) {
+                        if (newValue != null) {
+                            selectedAdvisor = (Advisor) newValue;
+                        }
+                        selectedAdvisorListener();
+                    }
+                });
+            }
+        };
+        nisitTable.getTableView().getColumns().clear();
+        nisitTable.getTableView().getItems().clear();
+        nisitTable.addColumn("รหัสอาจารย์","id");
+        nisitTable.addColumn("ชื่อ-นามสกุล","name");
+        nisitTable.addColumn("อีเมล","email");
+        nisitTable.addColumn("คณะ","faculty");
+        nisitTable.addColumn("ภาควิชา","department");
+        nisitTable.addStyleSheet("/ku/cs/styles/department/pages/nisit-advisor-management/department-nisit-advisor-management-advisor-table-stylesheet.css");
+    }
+    private void refreshNisitTable(){
+        nisitTableView.getItems().clear();
+        nisitList = nisitDatasource.readData();
+        UserList filterList;
+        if(session != null && session.getUser() != null){
+            filterList = new UserList();
+            String currentDepartment = ((DepartmentUser)session.getUser()).getDepartment();
+            for(User user : nisitList.getUsers("student")){
+                if(((Student)user).getDepartment().equalsIgnoreCase(currentDepartment)){
+                    try {
+                        filterList.addUser(user);
+                    } catch (UserException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else{
+            filterList = nisitList;
+        }
+
+        for(User user : filterList.getUsers("student")){
+            if(user.isRole("student")){
+                nisitTableView.getItems().add(user);
+            }
+        }
+        nisitTableView.getSortOrder().clear();
+        TableColumn nisitCol = nisitTableView.getColumns().get(0);
+        nisitCol.setSortable(true);
+        nisitTableView.getSortOrder().add(nisitCol);
+        nisitCol.setSortType(TableColumn.SortType.ASCENDING);
+        nisitTableView.sort();
+        nisitCol.setSortable(false);
+    }
+    private void refreshAdvisorTable(){
+        advisorTableView.getItems().clear();
+        advisorList = advisorDatasource.readData();
+        UserList filterList;
+        if(session != null && session.getUser() != null){
+            filterList = new UserList();
+            String currentDepartment = ((DepartmentUser)session.getUser()).getDepartment();
+            for(User user : advisorList.getUsers("advisor")){
+                if(((Advisor)user).getDepartment().equalsIgnoreCase(currentDepartment)){
+                    try {
+                        filterList.addUser(user);
+                    } catch (UserException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else{
+            filterList = advisorList;
+        }
+
+        for(User user : filterList.getUsers("advisor")){
+            if(user.isRole("advisor")){
+                advisorTableView.getItems().add(user);
+            }
+        }
+        advisorTableView.getSortOrder().clear();
+        TableColumn nisitCol = advisorTableView.getColumns().get(0);
+        nisitCol.setSortable(true);
+        advisorTableView.getSortOrder().add(nisitCol);
+        nisitCol.setSortType(TableColumn.SortType.ASCENDING);
+        advisorTableView.sort();
+        nisitCol.setSortable(false);
+    }
+    private void selectedStudentListener(){
+        if(selectedStudent != null){
+            changeShowNisitData();
+            showNisitVBox.setVisible(true);
+            showNisitVBox.setDisable(false);
+        }else{
+            showNisitVBox.setVisible(false);
+            showNisitVBox.setDisable(true);
+        }
+        completeSelectedListener();
+    }
+    private void selectedAdvisorListener(){
+        if(selectedAdvisor != null){
+            changeShowAdvisorData();
+            showAdvisorVBox.setVisible(true);
+            showAdvisorVBox.setDisable(false);
+        }else{
+            showAdvisorVBox.setVisible(false);
+            showAdvisorVBox.setDisable(true);
+        }
+        completeSelectedListener();
+    }
+    private void completeSelectedListener(){
+        if(selectedStudent != null && selectedAdvisor != null){
+            changeAdvisorButton.setDisable(false);
+        }else{
+            changeAdvisorButton.setDisable(true);
+        }
+    }
+    private void changeShowNisitData(){
+        nisitNameLabel.setText(selectedStudent.getName());
+        nisitIdLabel.setText(selectedStudent.getId());
+        UUID advisorUUID = selectedStudent.getAdvisor();
+        if(advisorUUID != null){
+            User currentAdvisor = advisorList.findUserByUUID(advisorUUID);
+            if(currentAdvisor != null){
+                nisitAdvisorNameLabel.setText(currentAdvisor.getName());
+                nisitAdvisorIdLabel.setText(currentAdvisor.getId());
+            }else{
+                nisitAdvisorNameLabel.setText("ไม่พบอาจารย์ที่ปรึกษาในระบบ");
+                nisitAdvisorIdLabel.setText("-");
+            }
+        }else{
+            nisitAdvisorNameLabel.setText("ไม่มีอาจารย์ที่ปรึกษา");
+            nisitAdvisorIdLabel.setText("-");
+        }
+        SquareImage nisitImage = new SquareImage(nisitImageView);
+        nisitImage.setClipImage(50,50);
+        if(!selectedStudent.getAvatar().equalsIgnoreCase("no-image")){
+            ImageDatasource imageDatasource = new ImageDatasource("users");
+            nisitImage.setImage(imageDatasource.openImage(selectedStudent.getAvatar()));
+        }
+    }
+    private void changeShowAdvisorData(){
+        advisorNameLabel.setText(selectedAdvisor.getName());
+        advisorIdLabel.setText(selectedAdvisor.getId());
+        SquareImage advisorImage = new SquareImage(advisorImageView);
+        advisorImage.setClipImage(50,50);
+        if(!selectedAdvisor.getAvatar().equalsIgnoreCase("no-image")){
+            ImageDatasource imageDatasource = new ImageDatasource("users");
+            advisorImage.setImage(imageDatasource.openImage(selectedAdvisor.getAvatar()));
+        }
+    }
+    private void onChangeAdvisor(){
+        try {
+            selectedStudent.setAdvisor(selectedAdvisor);
+            nisitDatasource.writeData(nisitList);
+//            refreshNisitTable();
+            nisitTableView.refresh();
+            selectedStudentListener();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void onRemoveLinkAdvisor(Student student){
+        try {
+            student.setAdvisor("no-advisor");
+            nisitDatasource.writeData(nisitList);
+//            refreshNisitTable();
+            nisitTableView.refresh();
+            selectedStudentListener();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private TableColumn<User,?> newStatusColumn(String colName){
+        TableColumn<User,Text> column = new TableColumn<>(colName);
+        column.setSortable(false);//BLOCK SORT BY CLICK
+        column.setReorderable(false);//BLOCK DRAG BY MOUSE
+        column.setCellFactory(c -> new TableCell<>(){
+            @Override
+            protected void updateItem(Text item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || getTableView() == null || getTableView().getItems().get(getIndex()) == null) {
+                    setGraphic(null);
+                } else {
+                    Student student = (Student) getTableView().getItems().get(getIndex());
+                    String status;
+                    if(student.getAdvisor() != null){
+                        User cuurentAdvisor = advisorList.findUserByUUID(student.getAdvisor());
+                        if(cuurentAdvisor != null){
+                            status = "มีที่ปรึกษา";
+                        }else{
+                            status = "มีที่ปรึกษา(!)";
+                        }
+                    }else{
+                        status = "ไม่มีที่ปรึกษา";
+                    }
+
+
+                    setGraphic(new Text(status));
+                }
+            }
+        });
+        return column;
+    }
+
+    private TableColumn<User,?> newDeleteColumn(){
+        TableColumn<User, HBox> column = new TableColumn<>("");
+        column.setSortable(false);//BLOCK SORT BY CLICK
+        column.setReorderable(false);//BLOCK DRAG BY MOUSE
+
+        column.setCellFactory(c -> new TableCell<>() {
+            private Button actionButton = new Button();
+            private final HBox hbox = new HBox(actionButton);
+            {
+                hbox.setAlignment(Pos.CENTER_LEFT);
+                hbox.setPrefSize(25,25);
+                DefaultButton b = new DefaultButton(actionButton,"transparent", "#e0e0e0", "#000000"){
+                    @Override
+                    protected void handleClickEvent() {
+                        button.setOnMouseClicked(e -> {
+                            User user = getTableView().getItems().get(getIndex());
+                            System.out.println("Remove link-advisor clicked for student: " + user.getId() + " " + user.getName());
+                            mainStackPane.getChildren().add(new ConfirmStack("ยืนยัน","คุณต้องการลบอาจารย์ที่ปรึกษาใช่มั้ย"){
+                                @Override
+                                protected void handleAcceptButton(){
+                                    getAcceptButton().setOnMouseClicked(e -> {
+                                        System.out.println("Accept button clicked");
+                                        mainStackPane.getChildren().removeLast();
+                                        onRemoveLinkAdvisor((Student) user);
+                                    });
+                                }
+                                @Override
+                                protected void handleDeclineButton(){
+                                    getDeclineButton().setOnMouseClicked(e -> {
+                                        System.out.println("Decline button clicked");
+                                        mainStackPane.getChildren().removeLast();
+                                    });
+                                }
+                            });
+
+                        });
+                    }
+                };
+                Image deleteButtonImage = new Image(getClass().getResourceAsStream("/images/pages/department/global/red-bin.png"));
+
+                b.changeBackgroundRadius(20);
+                b.setImage(deleteButtonImage,25,25);
+            }
+            @Override
+            protected void updateItem(HBox item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || getTableView() == null || getTableView().getItems().get(getIndex()) == null) {
+                    setGraphic(null); // No content for empty cells
+                } else {
+                    Student student = (Student) getTableView().getItems().get(getIndex());
+                    if(student.getAdvisor() != null){
+                        hbox.setDisable(false);
+                    }else{
+                        hbox.setDisable(true);
+                    }
+                    setGraphic(hbox); // Set the HBox with the button as the cell's graphic
+                }
+            }
+        });
+        return column;
     }
     private void initShowVBox(VBox vbox){
 //        vbox.setSpacing(5);
