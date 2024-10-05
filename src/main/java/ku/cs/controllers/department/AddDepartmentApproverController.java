a9370fa2342298a519f05673558ef1379a030535
Tanaanan <mjsalyjoh@gmail.com>
2024-10-06 01:20:48 +0700

    add choicebox and optional label to AddDepartmentApproverController.java

--- src/main/java/ku/cs/controllers/department/AddDepartmentApproverController.java
+++ src/main/java/ku/cs/controllers/department/AddDepartmentApproverController.java
@@ -1,6 +1,8 @@
 package ku.cs.controllers.department;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -30,6 +32,33 @@ public class AddDepartmentApproverController {
     private String approverType = "approver";
     private Request request;
 
+    @FXML
+    private Label optionalRoleTextField;
+
+    @FXML
+    private ChoiceBox<String> roleChoiceBox;
+
+    private String[] roles = {"หัวหน้าภาควิชา", "รองหัวหน้าภาควิชา", "รักษาการณ์แทนหัวหน้าภาควิชา",
+            "คณบดี", "รองคณบดี", "รักษาการณ์แทนคณบดี", "อื่น ๆ"};
+
+    @FXML
+    public void initialize(){
+        academicRoleTextField.setVisible(false);
+        optionalRoleTextField.setVisible(false);
+        roleChoiceBox.setItems(FXCollections.observableArrayList(roles));
+        roleChoiceBox.setValue(roles[0]);
+
+        roleChoiceBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.equals("อื่น ๆ")) {
+                optionalRoleTextField.setVisible(true);
+                academicRoleTextField.setVisible(true);
+            } else {
+                optionalRoleTextField.setVisible(false);
+                academicRoleTextField.setVisible(false);
+            }
+        });
+    }
+
     public void setStage(Stage stage) {
         this.stage = stage;
     }
@@ -38,6 +67,13 @@ public class AddDepartmentApproverController {
         String name = nameTextField.getText().trim();
         String lastName = lastnameTextField.getText().trim();
         String academicRole = academicRoleTextField.getText().trim();
+
+        if (roleChoiceBox.getValue().equals("อื่น ๆ")) {
+            academicRole = academicRoleTextField.getText().trim();
+        } else {
+            academicRole = roleChoiceBox.getValue();
+        }
+
 
         if (!name.isEmpty() && !lastName.isEmpty() && !academicRole.isEmpty()) {
             errorLabel.setVisible(false);
